02e91da71cb92789332621e90f53f742a8d07952
zentia <363476658@qq.com>
2020-01-25 12:35:15 +0800

    Blending Cell Colors

---
+++ Assets/Hex Map/Scripts/HexDirection.cs
@@ -0,0 +1,28 @@
+﻿public enum HexDirection
+{
+    NE, // 东北 
+    E, // 东 
+    SE, // 东南
+    SW, // 西南 
+    W, // 西
+    NW, // 西北
+}
+
+public static class HexDirectionExtensions
+{
+    
+    public static HexDirection Opposite(this HexDirection direction)
+    {
+        return (int) direction < 3 ? (direction + 3) : (direction - 3);
+    }
+
+    public static HexDirection Previous(this HexDirection direction)
+    {
+        return direction == HexDirection.NE ? HexDirection.NW : (direction - 1);
+    }
+
+    public static HexDirection Next(this HexDirection direction)
+    {
+        return direction == HexDirection.NW ? HexDirection.NE : (direction + 1);
+    }
+}
